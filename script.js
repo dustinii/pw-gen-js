fb5eab51d66ed278169d0b89edd52ec721f8de09
dustinii <125335987+dustinii@users.noreply.github.com>
2023-06-28 18:16:50 -0700

    added hard coded array for special characters, added Array.from() functions for numbers and lower&upper case letters

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// Array of Special Characters to itterate and choose from during password generation
+const specialCharacters = [
+  '@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
+];
+
+// Generate number and letter arrays instead of hard coding characters
+const numericCharacters = Array.from({ length: 10 }, (_, i) => i.toString());
+const lowerCasedCharacters = Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 97));
+const upperCasedCharacters = Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 65));
+
+
+// Function to choose random characters from arrays generated during Array.from() methods
+const getRandom = (arr) => {
+  const randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
+};
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
